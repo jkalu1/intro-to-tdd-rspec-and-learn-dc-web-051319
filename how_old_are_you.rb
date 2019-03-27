5fb3911f5db1414552687c3445323ccdf3d37bd0
upbeat-changelog-8230 <jkalu1000@gmail.com>
2019-03-27 15:54:47 +0000

    Automatically backed up by Learn

---
+++ how_old_are_you.rb
@@ -0,0 +1,8 @@
+reqire_relative './current_age_for_bi_rth_year.rb'
+
+puts "What year were you born?"
+birth_year= gets.to_i
+
+users_age=current_age_for_bi_rth_year(birth_year)
+
+puts "You are:" + users_age.to_s + "years old."
